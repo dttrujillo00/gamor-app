7f8e76c3a2b8dfdfc999a5064634c5d4ec4d6a74
Daniel Tamayo <dttrujillo00@gmail.com>
2024-02-11 14:48:04 -0800

    Navbar responsive

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -2,30 +2,43 @@ import { NavLink } from 'react-router-dom'
 import { links } from './links'
 import { DarkLight } from '../DarkLight/DarkLight'
 import './Navbar.css'
-
+import { useState } from 'react'
 
 export const Navbar = () => {
 
-  return (
-    <nav className="navbar_container">
-        <ul>
-          {
-            links.map( link => {
-              return (
-                <li key={ link.title }>
-                  <NavLink 
-                    to={ link.to }
-                    className={({ isActive }) => isActive ? "ellipse_decorator purple active":""}
-                  >
-                    { link.title }
-                  </NavLink>
-                </li>
-              )
-            } )
-          }
-          <DarkLight />
-        </ul>
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-    </nav>
+  const showHideMenu = ({ target }) => {
+    console.log(target.classList)
+
+    setIsMenuOpen(!isMenuOpen)
+
+  }
+
+  return (
+
+    <div className='navbar_container'>
+      <i onClick={ showHideMenu } className={ isMenuOpen ? 'bx bx-x bx-sm' : 'bx bx-menu bx-sm' } id='icon_menu'></i>
+      <nav className={ isMenuOpen ? 'navbar show' : 'navbar' }>
+          <ul>
+            {
+              links.map( link => {
+                return (
+                  <li key={ link.title }>
+                    <NavLink 
+                      to={ link.to }
+                      className={({ isActive }) => isActive ? "ellipse_decorator purple active":""}
+                    >
+                      { link.title }
+                    </NavLink>
+                  </li>
+                )
+              } )
+            }
+            <DarkLight />
+          </ul>
+
+      </nav>
+    </div>
   )
 }
